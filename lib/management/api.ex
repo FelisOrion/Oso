52895b8ac339ff8b1c0a05f111f8a11aeca33d40
Cat <darkyfelis@gmail.com>
2017-10-21 21:01:25 +0200

    decoder, socket in view-layout

--- lib/management/api.ex
+++ lib/management/api.ex
@@ -4,17 +4,26 @@ defmodule Oso.Api do
   Module for API calls wrapping HTTPoison Base
   """
 
-  @expected_fields ~w()
-
-  def process_url(url, query) do
-    "https://test.ognisportoltre.it/api" <> url <>".json" <> query
+  def process_url(url) do
+    "https://test.ognisportoltre.it/api" <> url
   end
 
   def process_response_body(body) do
     body
-    |> Poison.decode!
-    |> Map.take(@expected_fields)
-    |> Enum.map(fn({k, v}) -> {String.to_atom(k), v} end)
+    |> Poison.decode!()
+    |> decoder()
   end
 
+  def decoder(value) when is_bitstring(value), do: value
+  def decoder(value) when is_number(value), do: value
+  def decoder(value) when is_boolean(value), do: value
+  def decoder(nil), do: ""
+  def decoder(value) when is_list(value) do
+    value
+    |> Enum.reduce([], fn(v, acc) -> acc ++ [decoder(v)] end)
+  end
+  def decoder(value) when is_map(value) do
+    value
+    |> Enum.reduce(%{}, fn({k, v}, acc) -> Map.put(acc, String.to_atom(k), decoder(v)) end)
+  end
 end
